ad01f51e204299487984528c9f8dccdeca97f32e
demetryous1999 <demetryous@outlook.es>
2021-10-11 22:01:47 -0500

    first

---
+++ src/sistemadeventas/Sistemadeventas.java
@@ -0,0 +1,11 @@
+package sistemadeventas;
+public class Sistemadeventas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
