d14d62af16bc63ee3f3f10274e99aac7be10c072
demetryous1999 <demetryous@outlook.es>
2021-10-11 22:07:35 -0500

    MODIFICADO FOTO

--- src/sistemadeventas/app.java
+++ src/sistemadeventas/app.java
@@ -38,6 +38,7 @@ public class app extends javax.swing.JFrame {
         productos1 = new javax.swing.JLabel();
         productos2 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
+        FOTO = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMaximumSize(new java.awt.Dimension(800, 600));
@@ -60,15 +61,22 @@ public class app extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(102, 102, 102));
 
+        FOTO.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/FERRETERIA.jpg"))); // NOI18N
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(FOTO))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 505, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(181, Short.MAX_VALUE)
+                .addComponent(FOTO)
+                .addGap(132, 132, 132))
         );
 
         javax.swing.GroupLayout panelappLayout = new javax.swing.GroupLayout(panelapp);
@@ -154,6 +162,7 @@ public class app extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel FOTO;
     private javax.swing.JLabel Reporte;
     private javax.swing.JLabel clientes;
     private javax.swing.JPanel jPanel1;
